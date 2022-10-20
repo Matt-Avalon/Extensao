605670442505573e2e6ec9ef4152898f7f90065c
Matt-Avalon <116230050+Matt-Avalon@users.noreply.github.com>
2022-10-20 01:02:26 +0000

    Criei as variáveis

---
+++ main.py
@@ -0,0 +1,16 @@
+# Meu primeiro Python!!!
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+# Quando quiser guardar uma String! (frase)
+nome = "Vinicius Lotto"
+#Quando quiser guardar um número inteirp
+Idade = 19
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+#Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
+#print("Minha idade é "+idade)
+print("Minha idade é "+idade)
