60e96cd1bc4fe3b8eb9ca037a0485e5c96888d34
Matt-Avalon <116230050+Matt-Avalon@users.noreply.github.com>
2022-10-26 23:14:17 +0000

    Comando de "input" e revisão do uso de "int" para tornar a variável inteira

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-# Meu primeiro Python!!!
-#
-# print() = comando de saida
-print("Alo mundo!")
+# comando input(): quero permitir que o usuário digite algo...
+nome = input ("Digite o seu nome: ")
+# comando de saída... exibir na tela: print (nome)
 
-# Quando quiser guardar uma String! (frase)
-nome = "Vinicius Lotto"
-#Quando quiser guardar um número inteirp
-idade = 19
+# f para colocar variáveis numa frase
+# errado: print ("Boa noite, seu nome é {nome}") 
+# certo
+print (f"Boa noite, seu nome é {nome}")
 
-#Exibir o meu nome (que está dentro da variável nome)
-print(nome)
+# pede idade (colocar int antes para a variável ser inteira/numérica)
+idade = int(input("Qual a sua idade?: "))
+print (f"Você tem {idade} anos")
 
-#Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variável idade
-#print("Minha idade é "+idade)
-print("Minha idade é "+str(idade))
-print(f"Minha idade é {idade}")
-print("Minha idade é {}".format(idade))
-
-#Quando quiser exibir "Meu nome é ... e tenho ... anos..." trocando pelas variáveis nome e idade
-print("Meu nome é {} e tenho {} anos".format(nome, idade))
+# e se eu quisesse mostrar o dobro da idade informada
+dobro = idade * 2
+print(f"O dobro da sua idade é: {dobro} anos")
